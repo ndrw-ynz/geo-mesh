dcdb58839f5ca97f08a0d9fbdc7095a6f4e0777d
ndrw-ynz <andrewyanza4@gmail.com>
2025-09-11 19:57:45 +0800

    feat: add MeshBuffer for managing VAO, VBO, and EBOs of meshes

---
+++ src/Core/MeshBuffer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <vector>
+#include <cstdint>
+
+#include "Common/Types.h"
+
+class MeshBuffer {
+  public:
+    void AllocateBuffers(size_t vertexCount, size_t indexCount);
+    void UpdateBuffers(std::vector<Vertex> &vertices, std::vector<uint32_t> &indices);
+    void DeleteBuffers();
+
+    const GLuint GetVAO() const { return m_VAO; }
+    const GLuint GetVBO() const { return m_VBO; }
+    const GLuint GetEBO() const { return m_EBO; }
+
+  private:
+    GLuint m_VAO;
+    GLuint m_VBO;
+    GLuint m_EBO;
+};
