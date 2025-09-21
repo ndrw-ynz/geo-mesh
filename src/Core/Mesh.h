74d91379f3baca0c4ae2a787490ecbedf206ac14
ndrw-ynz <andrewyanza4@gmail.com>
2025-09-21 16:58:54 +0800

    refactor(mesh): support creation of Meshes w/o indices

--- src/Core/Mesh.h
+++ src/Core/Mesh.h
@@ -7,6 +7,7 @@
 class Mesh {
   public:
     Mesh() = default;
+    Mesh(std::vector<Vertex> vertices);
     Mesh(std::vector<Vertex> vertices, std::vector<uint32_t> indices);
 
     std::vector<Vertex> &GetVertices() { return m_vertices; }
