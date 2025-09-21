e2830de84cc2e9a92aa2d23878e3d6fbd678a8d4
ndrw-ynz <andrewyanza4@gmail.com>
2025-09-21 22:10:52 +0800

    fix(shader): align vertex shader inputs with Vertex2D attributes for text rendering

--- data/shaders/text.vs.glsl
+++ data/shaders/text.vs.glsl
@@ -1,10 +1,11 @@
 #version 460 core
-layout(location = 0) in vec4 vertex;
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec2 aTex;
 
 uniform mat4 projection;
 
 out vec2 TexCoords;
 void main() {
-    gl_Position = projection * vec4(vertex.xy, 0, 1.0);
-    TexCoords = vertex.zw;
+    gl_Position = projection * vec4(aPos, 0, 1.0);
+    TexCoords = aTex;
 }
